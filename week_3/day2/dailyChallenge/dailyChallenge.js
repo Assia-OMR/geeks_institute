d4eda2405812e5460bd26070a1afa7c1ac578bfa
Aciya <assiaomr737@gmail.com>
2025-11-15 18:33:36 +0100

    adding daily challenge of week 3 day 2

---
+++ week_3/day2/dailyChallenge/dailyChallenge.js
@@ -0,0 +1,32 @@
+const planets = [
+  { name: "Mercury", color: "gray", moons: 0 },
+  { name: "Venus", color: "orange", moons: 0 },
+  { name: "Earth", color: "blue", moons: 1 },
+  { name: "Mars", color: "red", moons: 2 },
+  { name: "Jupiter", color: "brown", moons: 79 },
+  { name: "Saturn", color: "gold", moons: 83 },
+  { name: "Uranus", color: "lightblue", moons: 27 },
+  { name: "Neptune", color: "darkblue", moons: 14 }
+];
+
+const section = document.querySelector(".listPlanets");
+
+planets.forEach(planet => {
+  const planetDiv = document.createElement("div");
+  planetDiv.classList.add("planet");
+  planetDiv.style.backgroundColor = planet.color;
+
+  planetDiv.textContent = planet.name;
+
+  for (let i = 0; i < planet.moons; i++) {
+    const moon = document.createElement("div");
+    moon.classList.add("moon");
+
+    moon.style.left = `${120 + i * 20}px`;
+    moon.style.top = `${40 + i * 10}px`;
+
+    planetDiv.appendChild(moon);
+  }
+
+  section.appendChild(planetDiv);
+});
